33108177f6a755a283d4ebe1ac95d23b2e29e0ad
Lee Ann Rucker <lrucker@mac.com>
2022-12-06 14:04:08 -0800

    Initial Commit

---
+++ PTZ Backup/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  PTZ Backup
+//
+//  Created by Lee Ann Rucker on 12/6/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ PTZ Backup/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  PTZ Backup
+//
+//  Created by Lee Ann Rucker on 12/6/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
