7d4bdb2d20942db038d057630a11cb7d3961eff8
Lee Ann Rucker <lrucker@mac.com>
2022-12-06 17:30:25 -0800

    visca command support

--- PTZ Backup/AppDelegate.m
+++ PTZ Backup/AppDelegate.m
@@ -6,6 +6,14 @@
 //
 
 #import "AppDelegate.h"
+#import "libvisca.h"
+
+// yeah, yeah, globals bad.
+VISCAInterface_t iface;
+VISCACamera_t camera;
+
+BOOL open_interface(const char *ttydev);
+void close_interface(void);
 
 typedef enum {
     PTZRestore = 0,
@@ -13,7 +21,7 @@ typedef enum {
     PTZBackup = 2
 } PTZMode;
 
-NSString *cameraIPs[3] = {
+NSString *PTZCameraIPs[3] = {
     @"192.168.13.201",
     @"192.168.13.202",
     @"192.168.13.203"
@@ -21,11 +29,13 @@ NSString *cameraIPs[3] = {
 @interface AppDelegate ()
 
 @property (strong) IBOutlet NSWindow *window;
-@property NSInteger rangeOffset, currentIndex, cameraIndex;
+@property NSInteger rangeOffset, currentIndex;
+@property NSInteger openCamera, cameraIndex;
 @property NSInteger recallOffset, restoreOffset;
 @property (readonly) NSInteger recallValue, restoreValue;
 @property NSInteger currentMode;
 @property BOOL autoRecall;
+@property BOOL testMode;
 @end
 
 @implementation AppDelegate
@@ -51,15 +61,21 @@ NSString *cameraIPs[3] = {
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    self.openCamera = -1;
     self.rangeOffset = 80; // TODO: Defaults
     self.currentIndex = 1;
     self.cameraIndex = 0;
     [self updateMode:0]; // TODO: Defaults
+    [self loadCameraIfNeeded];
 }
 
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+    if (self.openCamera == self.cameraIndex) {
+        close_interface();
+        self.openCamera = -1;
+    }
 }
 
 
@@ -69,7 +85,7 @@ NSString *cameraIPs[3] = {
 
 - (NSString *)cameraIP {
     NSAssert(self.cameraIndex >= 0 && self.cameraIndex < 3, @"Camera index %ld out of range (0-3)", (long)self.cameraIndex);
-    return cameraIPs[self.cameraIndex];
+    return PTZCameraIPs[self.cameraIndex];
 }
 
 // memory_recall, memory_set
@@ -79,12 +95,31 @@ NSString *cameraIPs[3] = {
     return [NSString stringWithFormat:@"./visca_cli -d %@ memory_recall %ld\n./visca_cli -d %@ memory_set %ld", cameraIP, (long)self.recallValue, cameraIP, (long)self.restoreValue];
 }
 
+- (void)loadCameraIfNeeded {
+    if (self.openCamera == self.cameraIndex) {
+        return;
+    }
+    if (self.openCamera != -1) {
+        close_interface();
+        self.openCamera = -1;
+    }
+    if (!self.testMode) {
+        BOOL success = open_interface([[self cameraIP] UTF8String]);
+        if (success) {
+            self.openCamera = self.cameraIndex;
+        } else {
+            self.testMode = YES;
+        }
+    }
+}
+
 - (void)nextCamera {
     if (self.cameraIndex < 2) {
         self.cameraIndex = self.cameraIndex + 1;
     } else {
         self.cameraIndex = 0;
     }
+    [self loadCameraIfNeeded];
 }
 
 - (void)updateMode:(NSInteger)mode {
@@ -116,15 +151,25 @@ NSString *cameraIPs[3] = {
 }
 
 - (IBAction)recallScene:(id)sender {
-    NSString *cameraIP = [self cameraIP];
-    NSLog(@"./visca_cli -d %@ memory_recall %ld", cameraIP, (long)self.recallValue);
+    NSLog(@"./visca_cli -d %@ memory_recall %ld", [self cameraIP], (long)self.recallValue);
+    if (!self.testMode) {
+        [self loadCameraIfNeeded];
+        if (VISCA_memory_recall(&iface, &camera, self.recallValue) != VISCA_SUCCESS) {
+            NSLog(@"failed to recall scene %ld\n", self.recallValue);
+        }
 
+    }
 }
 
 - (IBAction)restoreScene:(id)sender {
-    NSString *cameraIP = [self cameraIP];
-    NSLog(@"./visca_cli -d %@ memory_set %ld", cameraIP, (long)self.restoreValue);
+    NSLog(@"./visca_cli -d %@ memory_set %ld", [self cameraIP], (long)self.restoreValue);
+    if (!self.testMode) {
+        [self loadCameraIfNeeded];
+        if (VISCA_memory_set(&iface, &camera, self.restoreValue) != VISCA_SUCCESS) {
+            NSLog(@"failed to restore scene %ld\n", self.restoreValue);
+        }
 
+    }
 }
 
 // NSTextField actions are required so we don't propagate return to the button, because it increments. But we may stil want to load the scene.
@@ -172,3 +217,38 @@ NSString *cameraIPs[3] = {
 }
 
 @end
+
+BOOL open_interface(const char *ttydev)
+{
+    int camera_num;
+    int port = 5678;
+    if (VISCA_open_tcp(&iface, ttydev, port) != VISCA_SUCCESS) {
+        NSLog(@"visca: unable to open tcp device %s:%d\n", ttydev, port);
+        return NO;
+    }
+
+    iface.broadcast = 0;
+    if (VISCA_set_address(&iface, &camera_num) != VISCA_SUCCESS) {
+        NSLog(@"visca: unable to set address\n");
+        VISCA_close(&iface);
+        return NO;
+    }
+
+    camera.address = 1;
+
+    if (VISCA_clear(&iface, &camera) != VISCA_SUCCESS) {
+        NSLog(@"visca: unable to clear interface\n");
+        VISCA_close(&iface);
+        return NO;
+    }
+
+    NSLog(@"Camera %s initialisation successful.\n", ttydev);
+    return YES;
+}
+
+void close_interface(void)
+{
+    //VISCA_usleep(2000); // calls usleep. doesn't appear to actually sleep.
+
+    VISCA_close(&iface);
+}
